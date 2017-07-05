8f5b6ef3d484ee97e7892a866f7d54d0ef368112
Jonathan Ginn <jon@wearebase.com>
2017-07-05 16:53:56 +0100

    Swap for loops inside of gulp tasks

--- index.js
+++ index.js
@@ -10,48 +10,47 @@ var configure = function (gulp, config) {
     require('gulp-help')(gulp);
 
     if (config.styles) {
-        for (var i = 0; i < config.styles.length; i++) {
-            var thisBuild = config.styles[i];
+        gulp.task('styles', 'Build stylesheets', function () {
+            for (var i = 0; i < config.styles.length; i++) {
+                var thisBuild = config.styles[i];
 
-            gulp.task('styles', 'Build stylesheets', function () {
-                return gulp.src(thisBuild.src)
-                .pipe(gulpPlugins.if(env.sourceMaps, gulpPlugins.sourcemaps.init()))
-                .pipe(gulpPlugins.sassGlob())
-                .pipe(
-                    gulpPlugins.sass({
-                        includePaths: thisBuild.includePaths
-                    }).on('error', gulpPlugins.sass.logError)
-                )
-                .pipe(
-                    gulpPlugins.autoprefixer({
-                        browsers: config.browsersList
-                    })
-                )
-                .pipe(gulpPlugins.csso())
-                .pipe(gulpPlugins.if(env.sourceMaps, gulpPlugins.sourcemaps.write(thisBuild.sourcemaps)))
-                .pipe(gulp.dest(thisBuild.dest));
-            }, {
-                aliases: ['stylesheets', 'css', 'scss']
-            });
-        }
+                gulp.src(thisBuild.src)
+                    .pipe(gulpPlugins.if(env.sourceMaps, gulpPlugins.sourcemaps.init()))
+                    .pipe(gulpPlugins.sassGlob())
+                    .pipe(
+                        gulpPlugins.sass({
+                            includePaths: thisBuild.includePaths
+                        }).on('error', gulpPlugins.sass.logError)
+                    )
+                    .pipe(
+                        gulpPlugins.autoprefixer({
+                            browsers: config.browsersList
+                        })
+                    )
+                    .pipe(gulpPlugins.csso())
+                    .pipe(gulpPlugins.if(env.sourceMaps, gulpPlugins.sourcemaps.write(thisBuild.sourcemaps)))
+                    .pipe(gulp.dest(thisBuild.dest));
+            }
+        }, {
+            aliases: ['stylesheets', 'css', 'scss']
+        });
     }
 
     if (config.javascript) {
-        for (var i = 0; i < config.javascript.length; i++) {
-            var thisBuild = config.javascript[i];
+        gulp.task('javascript', 'Compile Javascript', function () {
+            for (var i = 0; i < config.javascript.length; i++) {
+                var thisBuild = config.javascript[i];
 
-            gulp.task('javascript', 'Compile Javascript', function () {
-                return gulp.src(thisBuild.src)
-                .pipe(gulpPlugins.if(env.sourceMaps, gulpPlugins.sourcemaps.init()))
-                .pipe(gulpPlugins.include())
-                .pipe(gulpPlugins.uglify())
-                .pipe(gulpPlugins.if(env.sourceMaps, gulpPlugins.sourcemaps.write(thisBuild.sourcemaps)))
-                .pipe(gulp.dest(thisBuild.dest));
-            }, {
-                aliases: ['js']
-            });
-
-        }
+                gulp.src(thisBuild.src)
+                    .pipe(gulpPlugins.if(env.sourceMaps, gulpPlugins.sourcemaps.init()))
+                    .pipe(gulpPlugins.include())
+                    .pipe(gulpPlugins.uglify())
+                    .pipe(gulpPlugins.if(env.sourceMaps, gulpPlugins.sourcemaps.write(thisBuild.sourcemaps)))
+                    .pipe(gulp.dest(thisBuild.dest));
+            }
+        }, {
+            aliases: ['js']
+        });
     }
 
     gulp.task('watch', 'Watch for changes', function() {
